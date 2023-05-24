1ca960814eabdafe808e335d0c8dc9aabc85e341
Nicolò Fretti <34170904+nicofretti@users.noreply.github.com>
2023-05-24 19:15:48 +0200

    First film query

--- backend/src/resolvers.ts
+++ backend/src/resolvers.ts
@@ -6,5 +6,24 @@ export const resolvers = {
         numberSeven() {
             return 7;
         },
+        filmList(){
+            return [{
+                film_id: 1,
+                title: "title",
+                description: "description",
+                release_year: 2021,
+                language: "language",
+                rental_duration: 1,
+                rental_rate: 1.0,
+                length: 1,
+                replacement_cost: 1.0,
+                rating: "PG",
+                last_update: "last_update",
+                special_features: ["special_features"],
+                fulltext: "fulltext",
+                categories: ["categories"],
+                actors: ["actors"],
+            }]
+        }
     },
 };
