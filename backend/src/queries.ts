0bda3253a5fa3dea5643bd87a540c81cc7f2ade6
ledjo <ledjo.lleshaj@gmail.com>
2023-05-27 16:01:49 +0200

    More refactoring

--- backend/src/queries.ts
+++ backend/src/queries.ts
@@ -1,15 +1,5 @@
-import pg from 'pg'
 import { Film } from './types/film'
-
-const { Pool } = pg
-
-const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'dvdrental',
-    password: 'postgres',
-    port: 5432,
-})
+import { pool } from './services/database.js'
 
 export async function getFilmList(): Promise<[Film]> {
     const response = await pool.query(`SELECT * FROM film`)
