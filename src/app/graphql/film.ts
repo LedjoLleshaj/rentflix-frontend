f06c7d55c834650c061df24f5d580c1bdfb963b6
Ledjo Lleshaj <ledjolleshaj@yahoo.com>
2023-06-04 17:23:11 +0200

    Added query to get films for the table

---
+++ src/app/graphql/film.ts
@@ -0,0 +1,27 @@
+import { gql } from 'apollo-angular';
+
+export interface FilmModel {
+  title: string;
+  year?: number;
+  rating?: string;
+  category?: string;
+  language?: string;
+  cost: number;
+}
+
+export interface FilmListModel {
+  filmList: FilmModel[];
+}
+
+export const FILMLIST_QUERY = gql`
+  query filmList {
+    filmList {
+      title
+      year
+      rating
+      category
+      language
+      ycost
+    }
+  }
+`;
