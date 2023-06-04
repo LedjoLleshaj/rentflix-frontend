254ef8e98ae4d2e548fc059d54b975c011c42aa7
Nicolò Fretti <34170904+nicofretti@users.noreply.github.com>
2023-06-04 15:27:21 +0200

    Init zoom in and zoom out

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,16 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
+
+  zoomPercentage = 100;
+
+  zoom(direciton: number) {
+    this.zoomPercentage += direciton;
+    let className = document.body.className;
+    className = className.replace(/rf-zoom-\d/g, '');
+    document.body.className += className + ` rf-zoom-${(this.zoomPercentage)}`;
+  }
 }
