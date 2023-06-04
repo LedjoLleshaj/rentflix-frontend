420ab032150668aa4cbd694371501460cf4fc2b5
Nicolò Fretti <34170904+nicofretti@users.noreply.github.com>
2023-06-04 11:11:19 +0200

    Clear layout, add dark and light mode

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -10,11 +10,32 @@ import { Router } from '@angular/router';
 export class LayoutComponent {
   mobileQuery: MediaQueryList;
   private _mobileQueryListener: () => void;
+  darkMode: boolean = true;
 
   constructor(public router: Router, changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
     this.mobileQuery = media.matchMedia('(max-width: 600px)');
     this._mobileQueryListener = () => changeDetectorRef.detectChanges();
     this.mobileQuery.addEventListener('change', this._mobileQueryListener);
+  }
+
+  ngOnInit(): void {
+    this.darkMode = localStorage.getItem('darkMode') === 'true';
+    if (this.darkMode && !document.body.classList.contains('rf-dark-theme')) {
+      document.body.classList.add('rf-dark-theme');
+      return;
+    } else {
+      document.body.classList.remove('rf-dark-theme');
+    }
+  }
+
+  toggleDarkMode() {
+    this.darkMode = !this.darkMode;
+    localStorage.setItem('darkMode', this.darkMode.toString());
+    if (this.darkMode) {
+      document.body.classList.add('rf-dark-theme');
+      return;
+    }
+    document.body.classList.remove('rf-dark-theme');
   }
 
   ngOnDestroy(): void {
