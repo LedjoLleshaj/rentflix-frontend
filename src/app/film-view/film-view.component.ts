dcb234b59521477a936d9f10c759625151169197
Nicolò Fretti <34170904+nicofretti@users.noreply.github.com>
2023-06-06 19:02:02 +0200

    Init generalization

--- src/app/film-view/film-view.component.ts
+++ src/app/film-view/film-view.component.ts
@@ -1,10 +1,55 @@
 import { Component } from '@angular/core';
+import { FilmColumn } from '../models/film.model';
 
 @Component({
   selector: 'app-film-view',
   templateUrl: './film-view.component.html',
   styleUrls: ['./film-view.component.scss']
 })
-export class FilmViewComponent {
 
+
+//['title', 'release_year', 'rating', 'category', 'language', 'rental_rate', 'rent']
+export class FilmViewComponent {
+  columns: FilmColumn[] = [
+    {
+      title: 'Title',
+      field: 'title',
+      sortable: true
+    },
+    {
+      title: 'Release Year',
+      field: 'release_year',
+      sortable: true
+    },
+    {
+      title: 'Rating',
+      field: 'rating',
+      sortable: true
+    },
+    {
+      title: 'Category',
+      field: 'category',
+      sortable: true
+    },
+    {
+      title: 'Language',
+      field: 'language',
+      sortable: true
+    },
+    {
+      title: 'Rental Rate',
+      field: 'rental_rate',
+      sortable: true
+    },
+    {
+      title: 'Rent',
+      field: 'rent',
+      sortable: true
+    }
+
+  ];
+
+  ngOnInit() {
+    console.log(this.columns);
+  }
 }
--- src/app/shared/components/film-table/film-table.component.ts
+++ src/app/shared/components/film-table/film-table.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
 import { NgIf, NgFor } from '@angular/common';
 import { MatTableModule } from '@angular/material/table';
 import { MatButtonModule } from '@angular/material/button';
@@ -6,6 +6,8 @@ import { FilmModel } from 'src/app/graphql/film';
 import { MatPaginatorModule, PageEvent } from '@angular/material/paginator';
 import { GetFilmsFilterInput } from 'src/app/graphql/film';
 import { GetFilmsService } from '../../services/get-films/get-films.service';
+import { FilmColumn } from '../../../models/film.model';
+
 
 @Component({
   selector: 'app-film-table',
@@ -13,42 +15,43 @@ import { GetFilmsService } from '../../services/get-films/get-films.service';
   styleUrls: ['./film-table.component.scss'],
   standalone: true,
   imports: [MatTableModule, MatButtonModule, MatPaginatorModule, NgIf, NgFor],
-  providers: [GetFilmsService],
+  providers: [GetFilmsService]
 })
 export class FilmTableComponent implements OnInit {
-  displayedColumns: string[] = ['title', 'release_year', 'rating', 'category', 'language', 'rental_rate', 'rent'];
-  dataSource: FilmModel[] = [];
-  clickedRows = new Set<FilmModel>();
+  @Input() inputColumns: FilmColumn[];
+  // dataSource: FilmModel[] = [];
+  // clickedRows = new Set<FilmModel>();
+  columns: string[] = [];
   paginatedData: FilmModel[] = [];
   total = 0;
 
-  constructor(private GetFilmsService: GetFilmsService) {}
 
   @ViewChild(MatPaginatorModule) paginator!: MatPaginatorModule;
 
   ngOnInit() {
-    this.GetFilmsService.getFilms({
-      page: 1,
-      filmPerPage: 10,
-    } as GetFilmsFilterInput).subscribe((data) => {
-      console.log(data);
-      this.total = data.getFilms.total;
-      this.dataSource = data.getFilms.films;
-      this.paginatedData = this.dataSource;
-    });
+    //this.GetFilmsService.getFilms({
+    //  page: 1,
+    //  filmPerPage: 10,
+    //} as GetFilmsFilterInput).subscribe((data) => {
+    //  console.log(data);
+    //  this.total = data.getFilms.total;
+    //  this.dataSource = data.getFilms.films;
+    //  this.paginatedData = this.dataSource;
+    //});
+    this.columns = this.inputColumns.map((column) => column.field);
   }
 
   rentMovie(row: any) {
-    console.log(row.title, row.year);
+    //console.log(row.title, row.year);
   }
 
   nextPage(event: PageEvent) {
-    this.GetFilmsService.getFilms({
-      page: event.pageIndex + 1,
-      filmPerPage: event.pageSize,
-    } as GetFilmsFilterInput).subscribe((data) => {
-      this.dataSource = data.getFilms.films;
-      this.paginatedData = this.dataSource;
-    });
+    //this.GetFilmsService.getFilms({
+    //  page: event.pageIndex + 1,
+    //  filmPerPage: event.pageSize,
+    //} as GetFilmsFilterInput).subscribe((data) => {
+    //  this.dataSource = data.getFilms.films;
+    //  this.paginatedData = this.dataSource;
+    //});
   }
 }
