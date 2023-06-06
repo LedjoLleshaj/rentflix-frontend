fd09c4a5fa191560e8f19ce74a2dc38883a08814
Ledjo Lleshaj <ledjolleshaj@yahoo.com>
2023-06-06 12:16:30 +0200

    Refactored all components using darkmode with the new service

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -2,11 +2,12 @@ import { MediaMatcher } from '@angular/cdk/layout';
 import { ChangeDetectorRef, Component, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { LOCAL_STORAGE_KEYS } from '../shared/constants';
+import { DarkModeService } from '../shared/services/darkMode/dark-mode.service';
 
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
-  styleUrls: ['./layout.component.scss']
+  styleUrls: ['./layout.component.scss'],
 })
 export class LayoutComponent {
   _mobileQueryListener: () => void;
@@ -16,7 +17,12 @@ export class LayoutComponent {
   firstName: string = '';
   lastName: string = '';
 
-  constructor(public router: Router, changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
+  constructor(
+    public router: Router,
+    changeDetectorRef: ChangeDetectorRef,
+    media: MediaMatcher,
+    private darkModeService: DarkModeService
+  ) {
     this.mobileQuery = media.matchMedia('(max-width: 600px)');
     this._mobileQueryListener = () => changeDetectorRef.detectChanges();
     this.mobileQuery.addEventListener('change', this._mobileQueryListener);
@@ -27,18 +33,11 @@ export class LayoutComponent {
     this.firstName = localStorage.getItem(LOCAL_STORAGE_KEYS.FIRST_NAME) || '';
     this.lastName = localStorage.getItem(LOCAL_STORAGE_KEYS.LAST_NAME) || '';
     // make the opposite of the current value cause toggleDarkMode() will negate it
-    this.darkMode = !(localStorage.getItem(LOCAL_STORAGE_KEYS.DARK_MODE) === 'true');
-    this.toggleDarkMode();
   }
 
   toggleDarkMode() {
+    this.darkModeService.toggleDarkMode();
     this.darkMode = !this.darkMode;
-    localStorage.setItem(LOCAL_STORAGE_KEYS.DARK_MODE, this.darkMode.toString());
-    if (this.darkMode) {
-      document.body.classList.add('rf-dark-theme');
-      return;
-    }
-    document.body.classList.remove('rf-dark-theme');
   }
 
   ngOnDestroy(): void {
@@ -46,14 +45,12 @@ export class LayoutComponent {
   }
 
   logout() {
-    Object.values(LOCAL_STORAGE_KEYS).forEach(key => {
-        if (key !== LOCAL_STORAGE_KEYS.DARK_MODE) {
-          localStorage.removeItem(key);
-        }
+    Object.values(LOCAL_STORAGE_KEYS).forEach((key) => {
+      if (key !== LOCAL_STORAGE_KEYS.DARK_MODE) {
+        localStorage.removeItem(key);
       }
-    );
+    });
     document.body.classList.remove('rf-dark-theme');
     this.router.navigate(['/login']);
   }
-
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -5,11 +5,12 @@ import { CreateUserQueryResponse, LOGIN_QUERY } from '../graphql/login';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { LOCAL_STORAGE_KEYS } from '../shared/constants';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { DarkModeService } from '../shared/services/darkMode/dark-mode.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
   // username: string = '';
@@ -17,20 +18,19 @@ export class LoginComponent implements OnInit {
   loginForm: FormGroup;
   darkMode: boolean = false;
 
-  constructor(private router: Router,
-              private apollo: Apollo,
-              private snackBar: MatSnackBar
-  ) {
-  }
+  constructor(
+    private router: Router,
+    private apollo: Apollo,
+    private snackBar: MatSnackBar,
+    private darkModeService: DarkModeService
+  ) {}
 
   ngOnInit() {
     this.loginForm = new FormGroup({
       username: new FormControl('', Validators.required),
-      password: new FormControl('', Validators.minLength(4))
+      password: new FormControl('', Validators.minLength(4)),
     });
     // make the opposite of the current value cause toggleDarkMode() will negate it
-    this.darkMode = !(localStorage.getItem(LOCAL_STORAGE_KEYS.DARK_MODE) === 'true');
-    this.toggleDarkMode();
   }
 
   get username() {
@@ -42,39 +42,32 @@ export class LoginComponent implements OnInit {
   }
 
   confirm() {
-    this.apollo.query<CreateUserQueryResponse>({
-      query: LOGIN_QUERY,
-      variables: {
-        username: this.username?.value,
-        password: this.password?.value
-      }
-    }).subscribe({
-      next: ({ data }) => {
-        const token = data.login.token;
-        const userData = atob(token.split('.')[1]);
-        localStorage.setItem(LOCAL_STORAGE_KEYS.AUTH_TOKEN, data.login.token);
-        localStorage.setItem(LOCAL_STORAGE_KEYS.USERNAME, JSON.parse(userData).username);
-        localStorage.setItem(LOCAL_STORAGE_KEYS.FIRST_NAME, data.login.first_name);
-        localStorage.setItem(LOCAL_STORAGE_KEYS.LAST_NAME, data.login.last_name);
-        this.router.navigate(['/']);
-      },
-      error: (error) => {
-        this.snackBar.open('Username or password not correct');
-      }
-    });
-
+    this.apollo
+      .query<CreateUserQueryResponse>({
+        query: LOGIN_QUERY,
+        variables: {
+          username: this.username?.value,
+          password: this.password?.value,
+        },
+      })
+      .subscribe({
+        next: ({ data }) => {
+          const token = data.login.token;
+          const userData = atob(token.split('.')[1]);
+          localStorage.setItem(LOCAL_STORAGE_KEYS.AUTH_TOKEN, data.login.token);
+          localStorage.setItem(LOCAL_STORAGE_KEYS.USERNAME, JSON.parse(userData).username);
+          localStorage.setItem(LOCAL_STORAGE_KEYS.FIRST_NAME, data.login.first_name);
+          localStorage.setItem(LOCAL_STORAGE_KEYS.LAST_NAME, data.login.last_name);
+          this.router.navigate(['/']);
+        },
+        error: (error) => {
+          this.snackBar.open('Username or password not correct');
+        },
+      });
   }
-
 
   toggleDarkMode() {
+    this.darkModeService.toggleDarkMode();
     this.darkMode = !this.darkMode;
-    localStorage.setItem(LOCAL_STORAGE_KEYS.DARK_MODE, this.darkMode.toString());
-    if (this.darkMode) {
-      document.body.classList.add('rf-dark-theme');
-      return;
-    }
-    document.body.classList.remove('rf-dark-theme');
   }
-
-
 }
