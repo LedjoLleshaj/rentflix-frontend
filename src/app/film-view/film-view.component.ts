59f03f955818c5048562a3eba0445b09ee666f89
Ledjo Lleshaj <ledjolleshaj@yahoo.com>
2023-06-07 15:17:44 +0200

    Search bar input done... TODO api and css maybe

--- src/app/film-view/film-view.component.ts
+++ src/app/film-view/film-view.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-film-view',
   templateUrl: './film-view.component.html',
-  styleUrls: ['./film-view.component.scss']
+  styleUrls: ['./film-view.component.scss'],
 })
-export class FilmViewComponent {
+export class FilmViewComponent implements OnInit {
+  searchTitle = '';
 
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  onSearchTitleChange(event: any) {
+    this.searchTitle = event.target.value;
+    console.log(this.searchTitle);
+  }
 }
