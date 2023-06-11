1a73a20e0be25ca96d6f20e183623f7395fc27f3
Nicolò Fretti <34170904+nicofretti@users.noreply.github.com>
2023-06-11 11:43:32 +0200

    Tables fix

--- src/app/history-view/history-view.component.ts
+++ src/app/history-view/history-view.component.ts
@@ -4,13 +4,15 @@ import { GetRentalFilterInput, Rental } from '../models/rental.model';
 import { PageEvent } from '@angular/material/paginator';
 import { CardInput } from '../shared/components/stat-card/stat-card.component';
 import { MatDialog } from '@angular/material/dialog';
-import { RentalDetailsDialogComponent } from '../shared/components/rental-details-dialog/rental-details-dialog.component';
+import {
+  RentalDetailsDialogComponent
+} from '../shared/components/rental-details-dialog/rental-details-dialog.component';
 
 @Component({
   selector: 'app-history-view',
   templateUrl: './history-view.component.html',
   styleUrls: ['./history-view.component.scss'],
-  providers: [RentalApiService],
+  providers: [RentalApiService]
 })
 export class HistoryViewComponent {
   data: Rental[];
@@ -19,12 +21,13 @@ export class HistoryViewComponent {
     page: 1,
     itemsPerPage: 10,
     orderBy: '',
-    sort: 'asc',
+    sort: 'asc'
   };
 
   @Output() cardData: CardInput[];
 
-  constructor(private rentalApiService: RentalApiService, private dialog: MatDialog) {}
+  constructor(private rentalApiService: RentalApiService, private dialog: MatDialog) {
+  }
 
   ngOnInit() {
     this.rentalApiService.getRentsOfCustomer(this.filter).subscribe((data) => {
@@ -38,26 +41,26 @@ export class HistoryViewComponent {
           icon: 'upcoming',
           stat: String(data.getUser.rental_stats.current_rentals),
           description: 'Rentals in progress',
-          iconColor: 'text-orange-400',
+          iconColor: 'text-orange-400'
         },
         {
           icon: 'wallet',
           stat: String(data.getUser.rental_stats.total_amount) + ' €',
           description: 'Total spendings (€)',
-          iconColor: 'text-green-700',
+          iconColor: 'text-green-400'
         },
         {
           icon: 'category',
           stat: data.getUser.rental_stats?.most_frequent_category?.name,
           description: 'Favorite category  ',
-          iconColor: 'text-cyan-700',
+          iconColor: 'text-cyan-400'
         },
         {
           icon: 'timeline',
           stat: String(data.getUser.rental_stats.total_rentals),
           description: 'Total rentals      ',
-          iconColor: 'text-violet-800',
-        },
+          iconColor: 'text-violet-400'
+        }
       ];
     });
   }
@@ -70,18 +73,14 @@ export class HistoryViewComponent {
       this.data = data.getRentals.rentals;
     });
   }
-
-  infoMovie(rent: Rental) {
-    console.log(rent);
-  }
-
+  
   infoRental(rental: Rental) {
     this.rentalApiService.getRental(rental.rental_id).subscribe((rental) => {
       this.dialog
         .open(RentalDetailsDialogComponent, {
           width: '532px',
-          data: rental,
-        })
+          data: rental
+        });
     });
   }
 
@@ -92,7 +91,7 @@ export class HistoryViewComponent {
       rental_date: 'rental_date',
       return_date: 'return_date',
       rental_period: 'rental_period',
-      address: 'store.address',
+      address: 'store.address'
     };
 
     this.filter.sort = this.filter.sort === 'asc' ? 'desc' : 'asc';
--- src/app/shared/components/history-table/history-table.component.ts
+++ src/app/shared/components/history-table/history-table.component.ts
@@ -11,18 +11,18 @@ import { Rental } from '../../../models/rental.model';
   templateUrl: './history-table.component.html',
   styleUrls: ['./history-table.component.scss'],
   standalone: true,
-  imports: [MatTableModule, MatButtonModule, MatPaginatorModule, NgIf, NgFor, MatSortModule, DatePipe],
+  imports: [MatTableModule, MatButtonModule, MatPaginatorModule, NgIf, NgFor, MatSortModule, DatePipe]
 })
 export class HistoryTableComponent {
   columns: string[] = ['title', 'rental_date', 'return_date', 'rental_period', 'address', 'amount', 'rental-info'];
   @Input() data: Rental[] = [];
   @Input() total: number = 0;
   @Output() nextPage: EventEmitter<any> = new EventEmitter();
-  @Output() infoMovie: EventEmitter<any> = new EventEmitter();
   @Output() infoRental: EventEmitter<any> = new EventEmitter();
   @Output() announceSortChange: EventEmitter<any> = new EventEmitter();
 
   @ViewChild(MatPaginatorModule) paginator!: MatPaginatorModule;
 
-  ngOnInit() {}
+  ngOnInit() {
+  }
 }
