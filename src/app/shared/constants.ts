169cce8bdd00fab43f8908b2fac672fd6ea37c1a
Ledjo Lleshaj <ledjolleshaj@yahoo.com>
2023-06-15 18:54:16 +0200

    Refactoring

--- src/app/shared/constants.ts
+++ src/app/shared/constants.ts
@@ -1,11 +1,10 @@
 export enum LOCAL_STORAGE_KEYS {
-  USER_ID = 'rf-user-id',
-  AUTH_TOKEN = 'rf-auth-token',
-  USERNAME = 'rf-username',
-  FIRST_NAME = 'rf-first-name',
-  LAST_NAME = 'rf-last-name',
-  DARK_MODE = 'rf-dark-mode'
+  USER_ID = "rf-user-id",
+  AUTH_TOKEN = "rf-auth-token",
+  USERNAME = "rf-username",
+  FIRST_NAME = "rf-first-name",
+  LAST_NAME = "rf-last-name",
+  DARK_MODE = "rf-dark-mode",
 }
-
 
 export const LINKS_PER_PAGE = 5;
